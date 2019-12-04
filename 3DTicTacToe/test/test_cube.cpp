e897aad71ac2a2b06b0b31c3db4e302253a06b0c
vszabo2 <54809586+vszabo2@users.noreply.github.com>
2019-12-04 17:03:29 -0600

    Enhance Cube tests

--- 3DTicTacToe/test/test_cube.cpp
+++ 3DTicTacToe/test/test_cube.cpp
@@ -4,22 +4,71 @@
 TEST_CASE("4x4x4 Cube") {
     Cube<int, int> arr(4);
 
-    int count = 64;
+    SECTION("GetSideLength") {
+        REQUIRE(arr.GetSideLength() == 4);
+    }
 
-    for (int i = 0; i < 4; ++i) {
-        for (int j = 0; j < 4; ++j) {
-            for (int k = 0; k < 4; ++k) {
-                arr[{i, j, k}] = count--;
+    SECTION("Setting and Getting") {
+        int count = 64;
+
+        for (int i = 0; i < 4; ++i) {
+            for (int j = 0; j < 4; ++j) {
+                for (int k = 0; k < 4; ++k) {
+                    arr[{i, j, k}] = count--;
+                }
+            }
+        }
+
+        count = 64;
+        for (int i = 0; i < 4; ++i) {
+            for (int j = 0; j < 4; ++j) {
+                for (int k = 0; k < 4; ++k) {
+                    CHECK(arr[{i, j, k}] == count);
+                    count--;
+                }
             }
         }
     }
 
-    count = 64;
-    for (int i = 0; i < 4; ++i) {
-        for (int j = 0; j < 4; ++j) {
-            for (int k = 0; k < 4; ++k) {
-                CHECK(arr[{i, j, k}] == count);
-                count--;
+    SECTION("Fill") {
+        arr.fill(99);
+
+        for (int i = 0; i < 4; ++i) {
+            for (int j = 0; j < 4; ++j) {
+                for (int k = 0; k < 4; ++k) {
+                    CHECK(arr[{i, j, k}] == 99);
+                }
+            }
+        }
+    }
+}
+
+TEST_CASE("0x0x0 Cube") {
+    // We mostly care that it doesn't crash here
+    Cube<int, int> arr(0);
+    REQUIRE(arr.GetSideLength() == 0);
+    arr.fill(7);
+}
+
+TEST_CASE("2x2x2 Cube of strings") {
+    Cube<std::string, char> arr(2);
+
+    for (char i = 0; i < 2; ++i) {
+        for (char j = 0; j < 2; ++j) {
+            for (char k = 0; k < 2; ++k) {
+                std::stringstream sstream;
+                sstream << '{' << i << ',' << j << ',' << k << '}';
+                arr[{i, j, k}] = sstream.str();
+            }
+        }
+    }
+
+    for (char i = 0; i < 2; ++i) {
+        for (char j = 0; j < 2; ++j) {
+            for (char k = 0; k < 2; ++k) {
+                std::stringstream sstream;
+                sstream << '{' << i << ',' << j << ',' << k << '}';
+                CHECK(arr[{i, j, k}] == sstream.str());
             }
         }
     }
